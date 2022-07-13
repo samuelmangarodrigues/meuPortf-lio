0f82aa8608669ff7e03a441a65dcc0992be911a9
samuelmangarodrigues <samuelmanga22222222@outlook.com>
2022-07-13 15:25:35 -0300

    feat:cards/MyTechs

--- meuport/src/components/LearnedTechs/index.tsx
+++ meuport/src/components/LearnedTechs/index.tsx
@@ -1,160 +1,151 @@
 import { Box, Flex, Text } from "@chakra-ui/react";
-import { FaDatabase } from "react-icons/fa";
-import { HiOutlineDesktopComputer } from "react-icons/hi";
+import { FcAcceptDatabase, FcMultipleDevices } from "react-icons/fc";
 
 function LearnedTechs() {
     return (
         <Flex flexDirection={["column", "column", "row"]}>
-            <Box
-                _hover={{
-                    background: "black",
-                    transition: "0.5s",
-                }}
-                cursor="pointer"
+            <Box                
                 width={300}
                 display="flex"
                 flexDirection="column"
                 alignItems="center"
-                background="blackAlpha.500"
+                background="white"
                 marginRight={["0", "5"]}
                 marginTop={4}
                 borderRadius={4}
-                padding={3}
             >
-                <HiOutlineDesktopComputer color="yellow" size={80} />
-                <Text color="yellow.500" fontFamily="Roboto" fontSize={32}>
+                <FcMultipleDevices color="red" size={100} />
+                <Text color="red" fontFamily="Roboto" fontSize={32} bg="red.100" padding={1} margin={3} borderRadius={5}>
                     Front End
                 </Text>
-                <Text
-                    fontFamily="Abel"
-                    color="white"
-                    fontSize={24}
-                    display="flex"
-                    alignItems="center"
-                >
-                    HTML5
-                </Text>
-                <Text
-                    fontFamily="Abel"
-                    color="white"
-                    fontSize={24}
-                    display="flex"
-                    alignItems="center"
-                >
-                    CSS3
-                </Text>
-                <Text
-                    fontFamily="Abel"
-                    color="white"
-                    fontSize={24}
-                    display="flex"
-                    alignItems="center"
-                >
-                    JavaScript
-                </Text>
+                <Box bg="green.50" width="100%" display="flex" alignItems="center" flexDirection="column" borderRadius={4}>
+                    <Text
+                        fontFamily="Abel"
+                        color="black"
+                        fontSize={24}
+                        display="flex"
+                        alignItems="center"
+                    >
+                        HTML5
+                    </Text>
+                    <Text
+                        fontFamily="Abel"
+                        color="black"
+                        fontSize={24}
+                        display="flex"
+                        alignItems="center"
+                    >
+                        CSS3
+                    </Text>
+                    <Text
+                        fontFamily="Abel"
+                        color="black"
+                        fontSize={24}
+                        display="flex"
+                        alignItems="center"
+                    >
+                        JavaScript
+                    </Text>
 
-                <Text
-                    fontFamily="Abel"
-                    color="white"
-                    fontSize={24}
-                    display="flex"
-                    alignItems="center"
-                >
-                    React Js
-                </Text>
-                <Text
-                    fontFamily="Abel"
-                    color="white"
-                    fontSize={24}
-                    display="flex"
-                    alignItems="center"
-                >
-                    Typescript
-                </Text>
-                <Text
-                    fontFamily="Abel"
-                    color="white"
-                    fontSize={24}
-                    display="flex"
-                    alignItems="center"
-                >
-                    MaterialUI
-                </Text>
-                <Text
-                    fontFamily="Abel"
-                    color="white"
-                    fontSize={24}
-                    display="flex"
-                    alignItems="center"
-                >
-                    ChakraUi
-                </Text>
+                    <Text
+                        fontFamily="Abel"
+                        color="black"
+                        fontSize={24}
+                        display="flex"
+                        alignItems="center"
+                    >
+                        React Js
+                    </Text>
+                    <Text
+                        fontFamily="Abel"
+                        color="black"
+                        fontSize={24}
+                        display="flex"
+                        alignItems="center"
+                    >
+                        Typescript
+                    </Text>
+                    <Text
+                        fontFamily="Abel"
+                        color="black"
+                        fontSize={24}
+                        display="flex"
+                        alignItems="center"
+                    >
+                        MaterialUI
+                    </Text>
+                    <Text
+                        fontFamily="Abel"
+                        color="black"
+                        fontSize={24}
+                        display="flex"
+                        alignItems="center"
+                    >
+                        ChakraUi
+                    </Text>
+                </Box>
             </Box>
             <Box
-                cursor="pointer"
-                _hover={{
-                    background: "black",
-                    transition: "0.5s",
-                }}
                 width={300}
                 display="flex"
                 flexDirection="column"
                 alignItems="center"
-                background="blackAlpha.500"
+                background="white"
                 marginRight={["0", "5"]}
                 marginTop={4}
                 borderRadius={4}
-                padding={3}
             >
-                <FaDatabase size={80} color="blue" />
-                <Text color="blue" fontFamily="Roboto" fontSize={32}>
+                <FcAcceptDatabase size={100} color="blue" />
+                <Text color="blue" fontFamily="Roboto" fontSize={32} bg="blue.100" padding={1} margin={3} borderRadius={5}>
                     Back End
                 </Text>
-                <Text
-                    fontFamily="Abel"
-                    color="white"
-                    fontSize={24}
-                    display="flex"
-                    alignItems="center"
-                >
-                    Python
-                </Text>
-                <Text
-                    fontFamily="Abel"
-                    color="white"
-                    fontSize={24}
-                    display="flex"
-                    alignItems="center"
-                >
-                    PostgresSQL
-                </Text>
-                <Text
-                    fontFamily="Abel"
-                    color="white"
-                    fontSize={24}
-                    display="flex"
-                    alignItems="center"
-                >
-                    NodeJs
-                </Text>
-                <Text
-                    fontFamily="Abel"
-                    color="white"
-                    fontSize={24}
-                    display="flex"
-                    alignItems="center"
-                >
-                    Express
-                </Text>
-                <Text
-                    fontFamily="Abel"
-                    color="white"
-                    fontSize={24}
-                    display="flex"
-                    alignItems="center"
-                >
-                    DJango
-                </Text>
+                <Box bg="green.50" width="100%" display="flex" alignItems="center" flexDirection="column" borderRadius={4} height={252}>
+                    <Text
+                        fontFamily="Abel"
+                        color="black"
+                        fontSize={24}
+                        display="flex"
+                        alignItems="center"
+                    >
+                        Python
+                    </Text>
+                    <Text
+                        fontFamily="Abel"
+                        color="black"
+                        fontSize={24}
+                        display="flex"
+                        alignItems="center"
+                    >
+                        PostgresSQL
+                    </Text>
+                    <Text
+                        fontFamily="Abel"
+                        color="black"
+                        fontSize={24}
+                        display="flex"
+                        alignItems="center"
+                    >
+                        NodeJs
+                    </Text>
+                    <Text
+                        fontFamily="Abel"
+                        color="black"
+                        fontSize={24}
+                        display="flex"
+                        alignItems="center"
+                    >
+                        Express
+                    </Text>
+                    <Text
+                        fontFamily="Abel"
+                        color="black"
+                        fontSize={24}
+                        display="flex"
+                        alignItems="center"
+                    >
+                        DJango
+                    </Text>
+                </Box>
             </Box>
         </Flex>
     );
